3b59a61f0d1876c3231a744282ba6d094b66f56d
robbradley5678 <robbradley5678@yahoo.com>
2023-07-26 11:40:18 +0100

    Updated animations

--- js/show-on-scroll.js
+++ js/show-on-scroll.js
@@ -1,7 +1,9 @@
+/* Fades in elements with "hidden-until-scroll" when in viewport */
+
 const observer = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
-			entry.target.classList.add('show');
+			entry.target.classList.add('show-on-scroll');
 		}
 	})
 });
